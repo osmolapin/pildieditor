4bfd99cc648da9628b6710c24489782e2cd33236
DaFancy <48165006+DaFancy@users.noreply.github.com>
2021-05-01 20:10:14 +0300

    Version 1.0 Programmist
    Võimalik on ainult muuta resolutsiooni ja kujundus on halb

---
+++ Pildieditimisprogramm/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Pildieditimisprogramm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BtnSelect = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.initialResolution = new System.Windows.Forms.Label();
+            this.xResInput = new System.Windows.Forms.TextBox();
+            this.yResInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(420, 200);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(348, 245);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Image files | *.jpg; *.jpeg; *.png;";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // BtnSelect
+            // 
+            this.BtnSelect.Location = new System.Drawing.Point(12, 99);
+            this.BtnSelect.Name = "BtnSelect";
+            this.BtnSelect.Size = new System.Drawing.Size(75, 23);
+            this.BtnSelect.TabIndex = 1;
+            this.BtnSelect.Text = "Vali pilt";
+            this.BtnSelect.UseVisualStyleBackColor = true;
+            this.BtnSelect.Click += new System.EventHandler(this.BtnSelect_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(12, 392);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 2;
+            this.BtnSave.Text = "Salvesta";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // initialResolution
+            // 
+            this.initialResolution.AutoSize = true;
+            this.initialResolution.Location = new System.Drawing.Point(420, 182);
+            this.initialResolution.Name = "initialResolution";
+            this.initialResolution.Size = new System.Drawing.Size(38, 15);
+            this.initialResolution.TabIndex = 3;
+            this.initialResolution.Text = "label1";
+            this.initialResolution.Visible = false;
+            // 
+            // xResInput
+            // 
+            this.xResInput.Location = new System.Drawing.Point(102, 271);
+            this.xResInput.Name = "xResInput";
+            this.xResInput.Size = new System.Drawing.Size(100, 23);
+            this.xResInput.TabIndex = 4;
+            // 
+            // yResInput
+            // 
+            this.yResInput.Location = new System.Drawing.Point(139, 332);
+            this.yResInput.Name = "yResInput";
+            this.yResInput.Size = new System.Drawing.Size(100, 23);
+            this.yResInput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.yResInput);
+            this.Controls.Add(this.xResInput);
+            this.Controls.Add(this.initialResolution);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.BtnSelect);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Label initialResolution;
+        private System.Windows.Forms.Button BtnSelect;
+        private System.Windows.Forms.TextBox xResInput;
+        private System.Windows.Forms.TextBox yResInput;
+    }
+}
+
---
+++ Pildieditimisprogramm/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pildieditimisprogramm
+{
+    public partial class Form1 : Form
+    {
+        //Globaalsed X ja Y väärtused mida ei muudeta peale väärtustamist
+        //Väärtus tuleb btnSelect_Click funkt.-st
+        int initialX, initialY;
+        bool havePic = false; //Kontrolliks kas pilt on valitud
+        bool haveMultiplePic = false; //Kontrolliks kas mitu pilti on valitud "havePic" on false siis
+        bool haveToConvert = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Faili valimise dialoogil "OK" vajutamisel näidatakse pilti
+        //local string picture - pildi faili asukoht
+        private void BtnSelect_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                foreach (String file in openFileDialog1.FileNames) 
+                {
+                    counter++;
+                }
+
+                if (counter > 1)
+                {
+                    pictureBox.ImageLocation = null;
+                    initialResolution.Text = "Piltide kuvamine ei ole võimalik kuna on valitud rohkem kui 1 pilt";
+                    initialResolution.Visible = true;
+                    havePic = false;
+                    haveMultiplePic = true;
+                }
+                else if (counter == 1)
+                {
+                    string picture = openFileDialog1.FileName;
+
+                    Image img = Image.FromFile(picture);
+                    initialX = img.Width;
+                    initialY = img.Height;
+
+                    pictureBox.ImageLocation = picture;
+
+                    initialResolution.Text = "Pildi resolutsioon: " + initialX + "x" + initialY;  //Kuvatakse algne pildi resolutsioon
+                    initialResolution.Visible = true;
+                    havePic = true;
+                }
+                else if (counter == 0) 
+                {
+                    havePic = false;
+                    haveMultiplePic = false;
+                } 
+            }
+        }
+        private int CalculateHeight(Bitmap bitmap, int widthToCalculate) 
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            return (height * widthToCalculate) / width;
+        }
+        //Avatakse pilt, salvestatakse tehakse "img"-st bitmap
+        //"img"-i bitmapist tehakse koopia millele saab lisada resolutsiooni
+        //salvestatakse fail nimega "file300.jpg"
+        //local string picture - pildi faili asukoht
+        //bitmap - "picture" bitmap & newBitmap - "bitmap" koopia
+        //funkt. vajab sisestada -> desiredX & desiredY - salvestatava pildi resolutsioon
+        private void SavePicture(Image img, int desiredX, int desiredY, int counter) 
+        {
+            int usedY;
+            using (Bitmap bitmap = (Bitmap)img)
+            {
+                if (haveToConvert)
+                {
+                    usedY = CalculateHeight(bitmap, desiredX);
+                }
+                else 
+                {
+                    usedY = desiredY;
+                }
+                using (Bitmap newBitmap = new Bitmap(bitmap, desiredX, usedY))
+                {
+                    newBitmap.Save("a" + counter + ".jpg");
+                }
+            }
+        }
+
+        //Plidi olemasolul salvestatakse pilt soovitud resolutsiooniga
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            //Vaadatakse kastides "x/yResInput" olevate soovitud suuruste väärtusi
+            Int32.TryParse(xResInput.Text, out int x);
+            Int32.TryParse(yResInput.Text, out int y);
+            //Kui üheski kastis ei ole midagi siis ei salvestata midagi
+            if (x == 0 && y == 0)
+            {
+                return;
+            }
+            //Kui x kastis on siis arvutatakse y väärtus
+            else if (y == 0) 
+            {
+                haveToConvert = true;
+            }
+
+            if (havePic)
+            {
+                string picture = openFileDialog1.FileName;
+                Image img = Image.FromFile(picture);
+                //Salvestatakse pildi x, y ja counter pannakse 1 - [faili nimesse tuleb 1]
+                SavePicture(img, x, y, 1);
+
+                haveToConvert = false;
+            }
+            else if (haveMultiplePic) 
+            {
+                foreach (String file in openFileDialog1.FileNames) 
+                {
+                    i++;
+                    Image img = Image.FromFile(file);
+                    //Salvestatakse pildi x, y ja counter pannakse i - [faili nimesse tuleb i] i suureneb iga pildiga mis on
+                    SavePicture(img, x, y, i);
+                }
+
+                haveToConvert = false;
+            }
+        }
+    }
+}
